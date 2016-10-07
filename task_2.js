790f969bbcba5f7ba4c8dc0c37c4adb6be8225ae
Owais Ramzan <owaiskiani65@gmail.com>
2016-10-07 20:25:27 +0500

    Initial realse

---
+++ task_2.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var http = require('http')
+var fs = require('fs');
+var router = require('./routes/task2');
+var app = express();
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.set('view engine', 'ejs');
+
+app.use('/I/want/title', router);
+
+app.use("*", function (req, res) {
+    res.render('404.ejs');
+});
+
+var httpServer = http.createServer(app);
+
+httpServer.listen(3000, function () {
+    console.log("server listening on port: 3000");
+});
